81df135d53c34233abedf36cbf9a92af80baebda
ALVAROVELAME <136656117+ALVAROVELAME@users.noreply.github.com>
2024-04-13 20:11:12 -0300

    numeros

---
+++ IndiomaDoSistema/src/main/java/com/mycompany/indiomadosistema/IndiomaDoSistema.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.indiomadosistema;
+
+
+
+/**
+ *
+ * @author alvar
+ */
+public class IndiomaDoSistema {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.language"));
+    }
+}
